bc2c45b99925097c9ea203cf9cc840af95cf4c45
Luís Freitas <microchipgnu@gmail.com>
2024-10-28 14:45:21 +0000

    Slippage to 5%

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -95,7 +95,7 @@ const app = new Elysia({ prefix: "/api", aot: false })
       tokenOut: tokenOutData,
       amountIn: quantity,
       swapTodos,
-      slippageTolerance: 0.03,
+      slippageTolerance: 0.05,
       AccountId: accountId,
       referralId: "mintbase.near",
     });
